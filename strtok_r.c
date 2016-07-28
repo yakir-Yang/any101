afc39fc71fbbf3cc090baf43e8f08e748b6e3f15
Yakir Yang <yakir.yang@qq.com>
2016-07-28 21:12:26 +0800

    C: 'strtok_r' useful API
    Signed-off-by: Yakir Yang <yakir.yang@qq.com>

---
+++ strtok_r.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	char *str1, *str2, *token, *subtoken;
+	char *saveptr1, *saveptr2;
+	int j;
+
+	/*
+	 * $ ./out/strtok_r 'a/bbb///cc;xxx:yyy:aas' ';:' '/'
+	 * 1: a/bbb///cc
+	 *  --> a
+	 *  --> bbb
+	 *  --> cc
+	 * 2: xxx
+	 *  --> xxx
+	 * 3: yyy
+	 *  --> yyy
+	 * 4: aas
+	 *  --> aas
+	 */
+
+	if (argc != 4) {
+		fprintf(stderr, "Usage: %s string delim subdelim\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	for (j = 1, str1 = argv[1]; ; j++, str1 = NULL) {
+		token = strtok_r(str1, argv[2], &saveptr1);
+		if (token == NULL)
+			break;
+		printf("%d: %s\n", j, token);
+
+		for (str2 = token; ; str2 = NULL) {
+			subtoken = strtok_r(str2, argv[3], &saveptr2);
+			if (subtoken == NULL)
+				break;
+			printf(" --> %s\n", subtoken);
+		}
+	}
+
+	exit(EXIT_SUCCESS);
+}
