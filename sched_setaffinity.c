2443c3f4b5ec8fb1648d6aea6ed07235283fa9fe
Yakir Yang <ykk@rock-chips.com>
2016-07-31 09:48:02 +0800

    Linux: 'sched_setaffinity' mutile-core API
    Signed-off-by: Yakir Yang <ykk@rock-chips.com>

---
+++ sched_setaffinity.c
@@ -0,0 +1,57 @@
+/* Short test program to test sched_setaffinity
+ * (which sets the affinity of processes to processors).
+ * Compile: gcc sched_setaffinity_test.c
+ *              -o sched_setaffinity_test -lm
+ * Usage: ./sched_setaffinity_test
+ *
+ * Open a "top"-window at the same time and see all the work
+ * being done on CPU 0 first and after a short wait on CPU 1.
+ * Repeat with different numbers to make sure, it is not a
+ * coincidence.
+ */
+
+#include <stdio.h>
+#include <math.h>
+#include <sched.h>
+
+double waste_time(long n)
+{
+	double res = 0;
+	long i = 0;
+
+	while (i < n * 200000) {
+		i++;
+		res += sqrt (i);
+	}
+
+	return res;
+}
+
+int main(void)
+{
+	cpu_set_t cpus;
+
+	CPU_ZERO(&cpus);
+
+	/* add processor 0 */
+	CPU_SET(0, &cpus);
+
+	/* bind process to processor 0 */
+	if (sched_setaffinity(0, sizeof(cpus), &cpus) < 0) {
+		perror("sched_setaffinity");
+	}
+
+	/* waste some time so the work is visible with "top" */
+	printf("result: %f\n", waste_time(8000));
+
+	/* process switches to processor 1 now */
+	CPU_SET(1, &cpus);
+	CPU_CLR(0, &cpus);
+
+	if (sched_setaffinity(0, sizeof(cpus), &cpus) < 0) {
+		perror("sched_setaffinity");
+	}
+
+	/* waste some more time to see the processor switch */
+	printf("result: %f\n", waste_time(8000));
+}
